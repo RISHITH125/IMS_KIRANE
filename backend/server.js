b53ae37a8665d0eeee580c901e9e5a640ed26af4
Prathik0042 <prathikprats@gmail.com>
2024-10-24 15:49:12 +0530

    editted backend

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const readline = require('readline');
-// const bcrypt = require('bcrypt');
 
 const { createUser } = require('./createUser.js');
 
 const { addProduct } = require('./addProduct.js')
 
-const pool = mysql.createPool({
+// ad hoc login 
+const genpool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 }).promise()
 
-createUser(pool); 
-addProduct(pool);
+// pool = createUser(genpool); 
+// console.log('hi');
+// console.log('hi')
+
+addProduct(genpool)
+
 // del_user(pool, 'ytrewq')
