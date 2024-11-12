0c1999f46a33ae00ab2a0835601f6c41231bbf98
Prathik0042 <prathikprats@gmail.com>
2024-11-12 14:55:09 +0530

    auth storename change

--- Frontend/src/pages/authentication.jsx
+++ Frontend/src/pages/authentication.jsx
@@ -338,7 +338,7 @@ const Authentication = () => {
               Store Name:
               <input
                 type="tel"
-                name="phoneNumber"
+                name="storename"
                 value={formData.storename}
                 onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50 hover:bg-gray-100 transition"
