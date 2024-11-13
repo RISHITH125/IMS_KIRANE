7d5010d8e4f8eda3eb7edcdd9177f1b55d3185a0
RISHITH125 <rishithp125@gmail.com>
2024-11-13 13:33:48 +0530

    products page init

--- Frontend/src/context/ProductsContext.jsx
+++ Frontend/src/context/ProductsContext.jsx
@@ -54,13 +54,13 @@ export const ProductsProvider = ({ children }) => {
           }
   
           const responseData = await response.json();
-  
+          console.log('Response data:', responseData);
           // Use the `response` variable to process data further
           if (responseData.result) {
             console.log('Fetched data successfully:', responseData.message);
-            const proddat=responseData.userdet
-            console.log(proddat)
-            setProductsData(proddat.userdet);
+            const proddat=responseData.message
+
+            setProductsData(proddat);
             localStorage.setItem('productsData', JSON.stringify(proddat));
           } else {
             console.warn('Data fetch was not successful:', responseData.message);
